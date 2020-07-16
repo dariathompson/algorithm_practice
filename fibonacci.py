25e592b38ae4e82c8d7562be77b5e7ae51287459
dariathompson <daria.kambalina@gmail.com>
2020-07-16 19:04:53 +0100

    updated original fibonacci to use lru_cache

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,6 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=1000)
 def fibonacci(n):
     if n == 1:
         return 1
@@ -6,7 +9,7 @@ def fibonacci(n):
     elif n > 2:
         return fibonacci(n-1) + fibonacci(n-2)
     
-for n in range(1, 33):
+for n in range(1, 100):
     print(fibonacci(n))
     
 if __name__ == "__main__":
