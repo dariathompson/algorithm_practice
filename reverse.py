5f0ddaa023cae566f84a4cff0f655028e3c76d59
dariathompson <daria.kambalina@gmail.com>
2020-07-16 13:53:29 +0100

    my reverse algorithm

--- reverse.py
+++ reverse.py
@@ -2,7 +2,13 @@ import datetime
   
 def create_arr(length):
     return list(range(1, length+1))
-                
+
+def my_reverse(arr):
+    new_arr = []
+    for i in range(1, len(arr)+1):
+        new_arr.append(arr[-i])
+    return new_arr
+                        
 def main(num):
     arr = create_arr(num)
     start = datetime.datetime.now()
@@ -12,3 +18,6 @@ def main(num):
     print(elapsed.microseconds)
 
 main(500000)
+
+my_arr = [1, 2, 3, 4, 5, 6]
+print(my_reverse(my_arr))
